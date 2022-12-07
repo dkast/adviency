c9cba214cf765cb77c2397bc48213976e15cd07e
Daniel Castillejo <devcastillejo@gmail.com>
2022-12-07 15:27:07 -0600

    Adds a little bit of snow

---
+++ components/SnowfallBackground.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Snowfall from "react-snowfall";
+
+const SnowfallBackground = () => {
+  return (
+    <Snowfall
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+      }}
+      color="#fed7aa"
+      speed={[0.5, 1.0]}
+      snowflakeCount={100}
+    />
+  );
+};
+
+export default SnowfallBackground;
