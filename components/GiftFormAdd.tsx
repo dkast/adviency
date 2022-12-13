ceb557c47706c8f4008fede1f26c5f0f4dffde5d
Daniel Castillejo <devcastillejo@gmail.com>
2022-12-12 20:50:18 -0600

    Adds modal

--- components/GiftFormAdd.tsx
+++ components/GiftFormAdd.tsx
@@ -2,12 +2,15 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 
+import Modal from "@/components/Modal";
 import { Gift } from "@/types/gift";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 interface GiftFormAddProps {
   onSubmit: (gift: Gift) => void;
 }
 
 const GiftFormAdd = ({ onSubmit }: GiftFormAddProps) => {
+  const [open, setOpen] = useState(false);
   const [giftDescription, setGiftDescription] = useState<string>("");
   const [giftQty, setGiftQty] = useState<number>(1);
 
@@ -28,8 +31,9 @@ const GiftFormAdd = ({ onSubmit }: GiftFormAddProps) => {
   };
 
   return (
-    <div className="flex gap-2 bg-emerald-500 p-3 text-white">
-      <input
+    <>
+      <div className="flex gap-2 bg-emerald-500 p-3 text-white">
+        {/* <input
         type="text"
         placeholder="Describe tu regalo"
         className="block w-full border-0 border-b placeholder:text-emerald-400 border-emerald-300 bg-transparent focus:border-emerald-200 focus:ring-0 sm:text-sm"
@@ -50,8 +54,86 @@ const GiftFormAdd = ({ onSubmit }: GiftFormAddProps) => {
         disabled={giftDescription === ""}
       >
         Añadir
-      </button>
-    </div>
+      </button> */}
+        <button
+          className="rounded-full border border-transparent bg-emerald-600 text-white px-3 py-1.5 hover:bg-emerald-800 disabled:bg-emerald-500 disabled:text-emerald-300 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+          type="button"
+          onClick={() => setOpen(true)}
+        >
+          Añadir regalo
+        </button>
+      </div>
+      <Modal open={open} setOpen={setOpen}>
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-xl">Agrega un regalo</h2>
+          <button
+            className="text-stone-500 hover:bg-stone-200 rounded-full"
+            onClick={() => setOpen(false)}
+          >
+            <XMarkIcon className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <label
+              className="block text-stone-600 font-medium text-sm mb-1"
+              htmlFor="gift"
+            >
+              Regalo
+            </label>
+            <input
+              type="text"
+              placeholder="Calcetines"
+              className="block w-full rounded-md border-stone-300 shadow-sm focus:border-emerald-500 focus:ring-emerald-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-stone-600 font-medium text-sm mb-1"
+              htmlFor="imageURL"
+            >
+              Imagen
+            </label>
+            <input
+              type="text"
+              placeholder="http://imagen..."
+              className="block w-full rounded-md border-stone-300 shadow-sm focus:border-emerald-500 focus:ring-emerald-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-stone-600 font-medium text-sm mb-1"
+              htmlFor="user"
+            >
+              Para
+            </label>
+            <input
+              type="text"
+              placeholder="Destinatario"
+              className="block w-full rounded-md border-stone-300 shadow-sm focus:border-emerald-500 focus:ring-emerald-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-stone-600 font-medium text-sm mb-1"
+              htmlFor="qty"
+            >
+              Cantidad
+            </label>
+            <input
+              type="number"
+              placeholder="1"
+              className="block w-1/4 rounded-md border-stone-300 shadow-sm focus:border-emerald-500 focus:ring-emerald-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <button className="w-full mt-2 rounded-lg border border-transparent shadow-lg shadow-emerald-300/60 bg-emerald-500 text-white px-3 py-2 hover:bg-emerald-600 hover:text-white disabled:bg-stone-200 disabled:text-stone-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2">
+              Guardar
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
---
+++ components/Modal.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { AnimatePresence, motion } from "framer-motion";
+
+const overlay = {
+  visible: {
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.2,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      ease: "easeIn",
+      duration: 0.1,
+    },
+  },
+};
+const dialog = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.2,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 40,
+    scale: 0.9,
+    transition: {
+      ease: "easeIn",
+      duration: 0.2,
+    },
+  },
+};
+
+type ModalProps = {
+  children: React.ReactNode;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Modal = ({ children, open, setOpen }: ModalProps) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <AnimatePresence>
+        {open && (
+          <Dialog.Portal forceMount>
+            <Dialog.Overlay asChild forceMount>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                variants={overlay}
+                className="fixed inset-0 z-50 min-h-screen bg-black bg-opacity-60 px-4 pt-4 pb-20 sm:block sm:p-0"
+              >
+                <div className="fixed inset-0 z-50 flex min-h-screen items-start justify-center overflow-y-auto px-4 py-20">
+                  <Dialog.Content asChild forceMount>
+                    <motion.div
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                      variants={dialog}
+                      className="relative inline-block w-full overflow-hidden rounded-xl bg-white px-4 pt-5 pb-4 text-left align-top shadow-xl outline-none sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle"
+                    >
+                      {children}
+                    </motion.div>
+                  </Dialog.Content>
+                </div>
+              </motion.div>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        )}
+      </AnimatePresence>
+    </Dialog.Root>
+  );
+};
+
+export default Modal;
